750c33b2b706a6b765d66bf1e4489e4f0c8d8216
Yi Xin <angyixin@gmail.com>
2024-01-15 11:24:20 +0800

    cards

--- src/app/exercises/Template.js
+++ src/app/exercises/Template.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Template = ({ title, subtitle, description, date, children }) => {
   return (
-    <div className="py-8 flex flex-col gap-y-6 ">
+    <div className="py-8 flex flex-col gap-y-6 justify-start items-start grow w-full ">
       <div className="flex flex-col gap-y-2">
         <p className="font-semibold text-zinc-500 dark:text-zinc-400 uppercase text-xs tracking-wide">
           Exercise {subtitle} <span className="ml-2 border-l px-2">{date}</span>
@@ -12,7 +12,9 @@ const Template = ({ title, subtitle, description, date, children }) => {
         </h2>{' '}
         {/* <p className="text-zinc-700">{description}</p> */}
       </div>
-      <div className="grow">{children}</div>
+      <div className="grow border rounded flex justify-center items-center w-full bg-zinc-50">
+        {children}
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,20 @@ import Template from '@/app/exercises/Template';
 
 export default function Home() {
   return (
-    <div className="flex grow py-8">
+    <div className="flex grow py-8 ">
       <div className="container mx-auto grow">
         <Template
           subtitle="1"
           date="14 Jan 2024"
-          title="Slider to control text size and and dynamic text display"
+          title="Typography Slider"
           description="The slider component that controls the text size with dynamic text input">
           <TypographySlider />
         </Template>
         <Template
-          subtitle="1"
+          subtitle="2"
           date="14 Jan 2024"
-          title="Slider to control text size and and dynamic text display"
-          description="The slider component that controls the text size with dynamic text input">
+          title="Loading card"
+          description="Card loading skeleton until the content is ready and loaded">
           <SkeletonCard />
         </Template>
       </div>
