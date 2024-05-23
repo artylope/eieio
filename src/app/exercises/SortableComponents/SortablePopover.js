24929cefbc9ccfc6e86417ec07a7b603707fa736
Yi Xin <angyixin@gmail.com>
2024-05-23 12:41:18 +0800

    padding

--- src/app/exercises/SortableComponents/SortablePopover.js
+++ src/app/exercises/SortableComponents/SortablePopover.js
@@ -5,7 +5,7 @@ const SortablePopover = ({ onRemove, onDuplicate, onClose }) => {
   return (
     <div className="z-50 absolute top-9 left-0 bg-white border shadow-md w-[12rem] rounded p-2 flex flex-col justify-start gap-y-2">
       <button
-        className="px-2 py-2 w-full flex justify-start items-center  cursor-pointer rounded text-zinc-500 hover:bg-zinc-100 gap-x-2"
+        className="px-3 py-2 w-full flex justify-start items-center  cursor-pointer rounded text-zinc-500 hover:bg-zinc-100 gap-x-2"
         onClick={() => {
           onRemove();
           onClose();
@@ -13,7 +13,7 @@ const SortablePopover = ({ onRemove, onDuplicate, onClose }) => {
         <X className=" w-4 h-4" /> Delete
       </button>
       <button
-        className="px-2 py-2 w-full flex justify-start items-center  cursor-pointer rounded text-zinc-500 hover:bg-zinc-100 gap-x-2"
+        className="px-3 py-2 w-full flex justify-start items-center  cursor-pointer rounded text-zinc-500 hover:bg-zinc-100 gap-x-2"
         onClick={() => {
           onDuplicate();
           onClose();
